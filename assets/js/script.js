f91ab3dad1017e3ce29af7a7ee50a7e6ff3fae8a
Zach Mutch <zkmutch@gmail.com>
2023-02-15 23:22:01 -0500

    added variables and more questions to script.js, added div sections for quiz and highscore to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,36 @@ let questions = [
         title: 'The condition in an if / else statement is enclosed within _____.',
         choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
         answer: 'parentheses'
+    },
+    {
+        title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        choices: ['<script src="xxx.js">, <script href="xxx.js">, <script name="xxx.js">', '<script src="xxx.html">'],
+        answer: '<script src="xxx.js">'
+    },
+    {
+        title: 'How can you add a comment in JavaScript?',
+        choices: ['/* This is a comment */', '// This is a comment', '<!-- This is a comment -->', '$ This is a comment'],
+        answer: '// This is a comment'
+    },
+    {
+        title: 'Which of the following is NOT a JavaScript keyword?',
+        choices: ['let', 'const', 'var', 'git'],
+        answer: 'git'
     }
 ]
 
-//hide start screen
+//variables for all elements by id
+let startButton = document.getElementById('start-btn');
 let startScreenEl = document.getElementById('start-screen');
+let timeRemainingEl = document.getElementById('time-remaining');
+let quizScreenEl = document.getElementById('quiz-screen');
+
+
+
+quizScreenEl.setAttribute("hidden", true);
+
+
+
+
+
+
