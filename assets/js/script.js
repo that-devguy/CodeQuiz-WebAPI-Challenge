4a89e0b13ca2c2a54449d2c37b0e59906d272d52
that devguy <zkmutch@gmail.com>
2023-02-16 15:25:56 -0500

    updated question variables and started startGame functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,49 @@
 //questions
 let questions = [
     {
-        title: 'Commonly used data types DO NOT include:',
-        choices: ['strings', 'booleans', 'alerts', 'numbers'],
-        answer: 'alerts'
+        question: 'Commonly used data types DO NOT include:',
+        answers: [
+            { text: 'strings', correct: false }, 
+            { text: 'booleans', correct: false },
+            { text: 'alerts', correct: true },
+            { text: 'numbers', correct: false }
+        ]
     },
     {
-        title: 'The condition in an if / else statement is enclosed within _____.',
-        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-        answer: 'parentheses'
+        question: 'The condition in an if / else statement is enclosed within _____.',
+        answers: [
+            { text: 'quotes', correct: false },
+            { text: 'curly brackets', correct: false },
+            { text: 'parentheses', correct: true },
+            { text: 'square brackets', correct: false }
+        ]
     },
     {
-        title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
-        choices: ['<script src="xxx.js">, <script href="xxx.js">, <script name="xxx.js">', '<script src="xxx.html">'],
-        answer: '<script src="xxx.js">'
+        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        answers: [
+            { text: '<script src="xxx.js">', correct: true },
+            { text: '<script href="xxx.js">', correct: false },
+            { text: '<script name="xxx.js">', correct: false },
+            { text: '<script src="xxx.html">', correct: false }
+        ]
     },
     {
-        title: 'How can you add a comment in JavaScript?',
-        choices: ['/* This is a comment */', '// This is a comment', '<!-- This is a comment -->', '$ This is a comment'],
-        answer: '// This is a comment'
+        question: 'How can you add a comment in JavaScript?',
+        answers: [
+            { text: '/* This is a comment */', correct: false },
+            { text: '// This is a comment', correct: true},
+            { text: '<!-- This is a comment -->', correct: false },
+            { text: '$ This is a comment', correct: false }
+        ]
     },
     {
-        title: 'Which of the following is NOT a JavaScript keyword?',
-        choices: ['let', 'const', 'var', 'git'],
-        answer: 'git'
+        question: 'Which of the following is NOT a JavaScript keyword?',
+        answers: [
+            { text: 'let', correct: false },
+            { text: 'const', correct: false },
+            { text: 'var', correct: false },
+            { text: 'git', correct: true }
+        ],
     }
 ]
 
@@ -33,9 +53,18 @@ let startScreenEl = document.getElementById('start-screen');
 let timeRemainingEl = document.getElementById('time-remaining');
 let quizScreenEl = document.getElementById('quiz-screen');
 
+let shuffledQuestions, currentQuestionIndex
 
 
-quizScreenEl.setAttribute("hidden", true);
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+    console.log('Started');
+    startScreenEl.style.display = 'none';
+    quizScreenEl.style.display = 'flex';
+    
+}
+
 
 
 
