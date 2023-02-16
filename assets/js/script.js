1aa7405cc4d3a831c5851eb0f6d279105bdb368c
Zach Mutch <zkmutch@gmail.com>
2023-02-15 20:21:21 -0500

    added some variables to script.js and added comments to html

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let questions = [
+    {
+        title: 'Commonly used data types DO NOT include:',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts'
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed within _____.',
+        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses'
+    }
+]
+
+//hide start screen
+let startScreenEl = document.getElementById('start-screen');
